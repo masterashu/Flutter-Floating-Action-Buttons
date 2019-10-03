8f69ac7fae87b5a3e5a1977b6772e5f5ecc67d73
Ashutosh Chauhan <masterashu@live.in>
2019-10-03 11:15:24 +0530

    adding button move animation.

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+        fontFamily: 'Quicksand',
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -27,16 +19,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -44,68 +26,106 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // Heading Bar of the APp
+      appBar: AppBar(),
 
-  void _incrementCounter() {
+      // Body of the App
+      body: Container(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+          child: Text("Hello"),
+        ),
+      ),
+      // Floating Button
+      floatingActionButton: MovingButtons(),
+    );
+  }
+}
+
+class MovingButtons extends StatefulWidget {
+  MovingButtons({Key key}) : super(key: key);
+
+  @override
+  MovingButtonsState createState() => MovingButtonsState();
+}
+
+class MovingButtonsState extends State<MovingButtons> {
+  bool x = false;
+
+  Widget build(BuildContext context) {
+    return AnimatedAlign(
+      duration: Duration(seconds: 1),
+      alignment: (x) ? Alignment.bottomRight : Alignment.centerRight,
+      child: FloatingActionButton(
+        onPressed: (x) ? _switchUp : _switchDown,
+        heroTag: 'goto_back',
+        child: (x) ? Icon(Icons.arrow_upward) : Icon(Icons.arrow_downward),
+      ),
+    );
+  }
+
+  void _switchUp() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      x = false;
     });
   }
 
-  @override
+  void _switchDown() {
+    setState(() {
+      x = true;
+    });
+  }
+}
+
+class _MovingButtonsState extends State<MovingButtons> {
+  // State Values
+  bool x = false;
+
+  // Builder
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    return Stack(
+      children: <Widget>[
+        Align(
+          alignment: (x) ? Alignment.bottomRight : Alignment.topRight,
+          child: Padding(
+            padding: (x) ? EdgeInsets.fromLTRB(30, 0, 0, 0) : EdgeInsets.fromLTRB(30, 200, 0, 0),
+            child: Transform.translate(
+              offset: Offset(0, 0),
+              child: FloatingActionButton(
+                onPressed: (x) ? null : _switchDown,
+                heroTag: 'goto_back',
+                child: Icon(Icons.arrow_downward),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Align(
+          alignment: (x) ? Alignment.bottomLeft : Alignment.topLeft,
+          child: Padding(
+            padding: (x) ? EdgeInsets.fromLTRB(30, 0, 0, 0) : EdgeInsets.fromLTRB(30, 200, 0, 0),
+            child: FloatingActionButton(
+              onPressed: (x) ? _switchUp : null,
+              heroTag: 'goto_front',
+              child: Icon(Icons.arrow_upward),
+            ),
+          ),
+        ),
+      ],
     );
+  }
+
+  // Functions
+  void _switchUp() {
+    setState(() {
+      x = false;
+    });
+  }
+
+  void _switchDown() {
+    setState(() {
+      x = true;
+    });
   }
 }
